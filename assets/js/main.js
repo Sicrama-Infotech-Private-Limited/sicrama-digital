423203146bd4aa79652f714700166a196dfe02df
Simran-Baheti <64544471+Simran-Baheti@users.noreply.github.com>
2022-05-16 13:55:20 +0530

    changes#6

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,11 +8,13 @@ $(document).ready(function () {
       $(".nav-link").css("color", "#004080");
       $(".logoblue").css("display", "block");
       $(".logowhite").css("display", "none");
+      $(".btn-outline-nodanger").css("color", "#004080");
     } else {
       $("#navbar").css("background", "transparent");
       $(".nav-link").css("color", "white");
       $(".logoblue").css("display", "none");
       $(".logowhite").css("display", "block");
+      $(".btn-outline-nodanger").css("color", "#fff");
     }
   });
 });
